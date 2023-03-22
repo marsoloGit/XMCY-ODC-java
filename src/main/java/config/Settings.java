3cbc2490aef0a41db8b9d258e5f7edbc1eb88f42
marsoloGit <marsoloGit@users.noreply.github.com>
2023-03-22 05:56:25 +0100

    new ui framework created

--- src/main/java/config/Settings.java
+++ src/main/java/config/Settings.java
@@ -4,7 +4,7 @@ package config;
 public class Settings {
     public final static String XMCY_URL = "https://xm.com";
     public final static Const.BrowserType BROWSER = Const.BrowserType.CHROME;
-    public static boolean BROWSER_HEADLESS = false ;
+    public static boolean BROWSER_HEADLESS = true ;
     public final static int WEBDRIVER_ELEMENT_WAIT = 60;
 }
 
